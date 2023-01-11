f1d6d070f92a7e492fad7d98e4c15e3af6bf5e9a
jackmeowmeow <122238982+jackmeowmeow@users.noreply.github.com>
2023-01-12 00:42:47 +0100

    Add files via upload

--- affcip.m
+++ affcip.m
@@ -1,20 +1,29 @@
-function encryptd = affcip(l,k,txt)
+function encryptd = affcip()
 
-% perform the affine cipher encryption of w using parameters lambda and kappa
-% l=lambda (note that lambda must be invertible mod36) 
-% k=kappa
-% Note that you can easily import plain text useble here by w = cell2mat(convertStringsToChars(importdata('File.txt')))
+    % perform the affine cipher encryption of a sentence using keys lambda and kappa
+    % l_key = lambda (note that lambda must be invertible mod36) 
+    % k_key = kappa
 
-if gcd(l,26)~=1 
-    error('Lambda l must be invertible mod26. Retry with a different number'); 
-else
-    index=('A':'Z'); %you need to choose the cipher
-    encryptd=txt;
+    disp('An affine cipher encrypt a phrase using a function l*x+k, where x is\n every single character in the phrase');
+    txt = input('Enter a phrase that you wish to encrypt it: ','s');
+
+    l_key = input ('\nEnter lambda: ');
+    if gcd(l_key,26)~=1 || isempty(l_key)
+        error('Lambda l must be invertible mod26. Retry with a different number'); 
+    end
+
+    k_key = input ('\nEnter kappa: ');
+    if isempty(k_key)
+        k_key = 0;
+    end
+
+    l_key = mod(l_key,26);
+    k_key = mod(k_key,26);
+    index=('a':'z'); %you need to choose the cipher
     for i=1:length(txt)
         if txt(i)~=' '&&txt(i)~='.'
             p = find(index==txt(i));
-            encryptd(i)=index(mod(l*(p-1)+k,26)+1);
+            encryptd(i)=index(mod(l_key*(p-1)+k_key,26)+1);
         end
     end
-end
 end
