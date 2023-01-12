d20e890e6618411200f531c25d85a6e8c4b8872e
jackmeowmeow <122238982+jackmeowmeow@users.noreply.github.com>
2023-01-12 01:41:19 +0100

    Add files via upload

--- countchar.m
+++ countchar.m
@@ -6,17 +6,17 @@ function [P,Q,T2,T3] = countchar(txt)
 % you want to modify it, you need to change the code.
 % T2 and T3 are the column of the two tables.
 
-index = 'A':'Z'; %you need to choose the cipher
+index = 'a':'z'; %you need to choose the cipher
 
-T1={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}';
+T1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}';
 T2 = zeros(26,1);
 for i=1:length(txt)
-    if txt(i)~=' '&&txt(i)~='.'
+    if isletter(txt(i))
         c = find(index==txt(i));
         T2(c) = T2(c)+1;
     end
 end
-P=table(T1,T2);
+P = table(T1,T2);
 T3 = (T2(:).*100)./length(txt);
-Q=table(T1,T3);
+Q = table(T1,T3);
 end
