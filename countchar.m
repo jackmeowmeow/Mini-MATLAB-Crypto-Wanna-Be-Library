7d39faa75cd81cf5d1bd86bed1078a90622848ff
jackmeowmeow <122238982+jackmeowmeow@users.noreply.github.com>
2023-01-12 01:57:22 +0100

    Add files via upload

--- countchar.m
+++ countchar.m
@@ -1,22 +1,24 @@
-function [P,Q,T2,T3] = countchar(txt)
+function [P,Freq,Perc_freq] = countchar(txt)
 
-% function that return a table P containing how many times a character appears in the text 
-% and a table Q containing the percentage of a character in a plain text.
-% txt must be a string. This only works with the classic affine cipher, if
-% you want to modify it, you need to change the code.
-% T2 and T3 are the column of the two tables.
+    % function that return a table P containing how many times a character appears in the text 
+    % and a table Q containing the percentage of a character in a plain text.
+    % txt must be a string. This only works with the classic affine cipher, if
+    % you want to modify it, you need to change the code.
+    % T2 and T3 are the column of the two tables.
 
-index = 'a':'z'; %you need to choose the cipher
+    index = 'a':'z'; %you need to choose the cipher
 
-T1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}';
-T2 = zeros(26,1);
-for i=1:length(txt)
-    if isletter(txt(i))
-        c = find(index==txt(i));
-        T2(c) = T2(c)+1;
+    Char = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}';
+    Freq = zeros(26,1);
+
+    iter = 0;
+    for i=1:length(txt)
+        if isletter(txt(i))
+            c = find(index==txt(i));
+            Freq(c) = Freq(c)+1;
+            iter = iter + 1;
+        end
     end
-end
-P = table(T1,T2);
-T3 = (T2(:).*100)./length(txt);
-Q = table(T1,T3);
+    Perc_freq = (Freq(:).*100)./iter;
+    P = table(Char,Freq,Perc_freq);
 end
